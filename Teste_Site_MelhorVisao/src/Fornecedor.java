11e586707ee3acbd97ef5df2554bf8e84fab2fb9
Michel Parada Ernst <781510023@senacinformatica.br>
2018-07-05 21:30:29 -0300

    Cadastro de fornecedor no banco de dados

--- Teste_Site_MelhorVisao/src/Fornecedor.java
+++ Teste_Site_MelhorVisao/src/Fornecedor.java
@@ -1,12 +1,64 @@
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
 
-class Fornecedor {
+import java.util.List;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Fornecedor {
+
+	
+	FirefoxDriver pagina;
+	
+	@Before
+	public void antes() {
+		
+		System.setProperty("webdriver.gecko.driver",
+				"C:\\Users\\781510023.SENACINFO\\eclipse\\java-oxygen\\eclipse\\geckodriver.exe");
+		
+		pagina = new FirefoxDriver();
+		pagina.get("http://localhost/cadastroFornecedor/frmFornecedor.php");
+		
+	}
+			
+			
 	@Test
-	void test() {
-		fail("Not yet implemented");
+ public	void test() {
+		
+		WebElement nome = pagina.findElementByName("nome");
+		nome.sendKeys("filipe");
+		WebElement cnpj = pagina.findElementByName("cnpj");
+		cnpj.sendKeys("444555666");
+		WebElement telefone = pagina.findElementByName("telefone");
+		telefone.sendKeys("(51)99988-8777");
+		
+		Select cidade = new Select(pagina.findElementByName("cidade")); 
+		cidade.selectByIndex(2);
+		
+		WebElement email = pagina.findElementByName("email");
+		email.sendKeys("adalto@gmail.com");
+		WebElement logradouro = pagina.findElementByName("logradouro");
+		logradouro.sendKeys("venancio");
+		WebElement numero = pagina.findElementByName("numero");
+		numero.sendKeys("4523");
+		WebElement complemento = pagina.findElementByName("complemento");
+		complemento.sendKeys("casa");
+		WebElement bairro = pagina.findElementByName("bairro");
+		bairro.sendKeys("cidade baixo");
+		WebElement cep = pagina.findElementByName("cep");
+		cep.sendKeys("91350-180");
+		
+		WebElement salvar = pagina.findElementByXPath("/html/body/form/input[11]");
+		salvar.click();
+	}
+	
+	public void depois() {
+		
+		
+		
 	}
 
 }
